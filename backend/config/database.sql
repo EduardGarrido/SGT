e7655f8927cf4aac0d78b5fd9ba0e991efc3208f
ElAguila <hernandez.jeg11@gmail.com>
2026-04-09 22:51:16 -0600

    DB query creation

---
+++ backend/config/database.sql
@@ -0,0 +1,241 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-12.2.2-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: sgtdb
+-- ------------------------------------------------------
+-- Server version	12.2.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `Caja`
+--
+
+DROP TABLE IF EXISTS `Caja`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Caja` (
+  `ID_Caja` int(11) NOT NULL AUTO_INCREMENT,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  `Monto_Inicial` float NOT NULL,
+  `Monto_Final` float DEFAULT NULL,
+  `Estado` varchar(20) DEFAULT NULL,
+  `Estado_Final` varchar(20) DEFAULT NULL,
+  `ID_Usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Caja`),
+  KEY `ID_Usuario` (`ID_Usuario`),
+  CONSTRAINT `1` FOREIGN KEY (`ID_Usuario`) REFERENCES `Usuario` (`ID_Usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Categoria`
+--
+
+DROP TABLE IF EXISTS `Categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Categoria` (
+  `ID_Categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre_Categoria` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID_Categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Contacto_Empleado`
+--
+
+DROP TABLE IF EXISTS `Contacto_Empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Contacto_Empleado` (
+  `ID_Contacto_Empleado` int(11) NOT NULL AUTO_INCREMENT,
+  `Telefono` varchar(20) DEFAULT NULL,
+  `Correo` varchar(255) DEFAULT NULL,
+  `Calle` varchar(100) DEFAULT NULL,
+  `Colonia` varchar(100) DEFAULT NULL,
+  `Codigo_Postal` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`ID_Contacto_Empleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Detalle_Surtir`
+--
+
+DROP TABLE IF EXISTS `Detalle_Surtir`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Detalle_Surtir` (
+  `ID_Detalle_Surtir` int(11) NOT NULL AUTO_INCREMENT,
+  `Cantidad` int(11) NOT NULL,
+  `Precio_Unitario` float NOT NULL,
+  `ID_Surtir` int(11) DEFAULT NULL,
+  `ID_Producto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Detalle_Surtir`),
+  KEY `ID_Surtir` (`ID_Surtir`),
+  KEY `ID_Producto` (`ID_Producto`),
+  CONSTRAINT `1` FOREIGN KEY (`ID_Surtir`) REFERENCES `Surtir` (`ID_Surtir`),
+  CONSTRAINT `2` FOREIGN KEY (`ID_Producto`) REFERENCES `Producto` (`ID_Producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Detalle_Venta`
+--
+
+DROP TABLE IF EXISTS `Detalle_Venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Detalle_Venta` (
+  `ID_Detalle_Venta` int(11) NOT NULL AUTO_INCREMENT,
+  `Cantidad` int(11) NOT NULL,
+  `Precio_Unitario` float NOT NULL,
+  `ID_Venta` int(11) DEFAULT NULL,
+  `ID_Producto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Detalle_Venta`),
+  KEY `ID_Venta` (`ID_Venta`),
+  KEY `ID_Producto` (`ID_Producto`),
+  CONSTRAINT `1` FOREIGN KEY (`ID_Venta`) REFERENCES `Venta` (`ID_Venta`),
+  CONSTRAINT `2` FOREIGN KEY (`ID_Producto`) REFERENCES `Producto` (`ID_Producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Empleado`
+--
+
+DROP TABLE IF EXISTS `Empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Empleado` (
+  `ID_Empleado` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) NOT NULL,
+  `Puesto` varchar(50) NOT NULL,
+  `Estado` varchar(20) NOT NULL,
+  `ID_Contacto_Empleado` int(11) DEFAULT NULL,
+  `ID_Usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Empleado`),
+  KEY `ID_Contacto_Empleado` (`ID_Contacto_Empleado`),
+  KEY `ID_Usuario` (`ID_Usuario`),
+  CONSTRAINT `1` FOREIGN KEY (`ID_Contacto_Empleado`) REFERENCES `Contacto_Empleado` (`ID_Contacto_Empleado`),
+  CONSTRAINT `2` FOREIGN KEY (`ID_Usuario`) REFERENCES `Usuario` (`ID_Usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Producto`
+--
+
+DROP TABLE IF EXISTS `Producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Producto` (
+  `ID_Producto` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre_Producto` varchar(255) NOT NULL,
+  `Precio` float NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Unidad_Medida` varchar(20) DEFAULT NULL,
+  `Estado` varchar(20) DEFAULT NULL,
+  `Cantidad_Minima` int(11) NOT NULL,
+  `ID_Categoria` int(11) DEFAULT NULL,
+  `ID_Proveedor` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Producto`),
+  KEY `ID_Categoria` (`ID_Categoria`),
+  KEY `ID_Proveedor` (`ID_Proveedor`),
+  CONSTRAINT `1` FOREIGN KEY (`ID_Categoria`) REFERENCES `Categoria` (`ID_Categoria`),
+  CONSTRAINT `2` FOREIGN KEY (`ID_Proveedor`) REFERENCES `Proveedor` (`ID_Proveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Proveedor`
+--
+
+DROP TABLE IF EXISTS `Proveedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Proveedor` (
+  `ID_Proveedor` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre_Proveedor` varchar(255) NOT NULL,
+  `Telefono` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID_Proveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Surtir`
+--
+
+DROP TABLE IF EXISTS `Surtir`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Surtir` (
+  `ID_Surtir` int(11) NOT NULL AUTO_INCREMENT,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  `Monto` float NOT NULL,
+  `ID_Proveedor` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Surtir`),
+  KEY `ID_Proveedor` (`ID_Proveedor`),
+  CONSTRAINT `1` FOREIGN KEY (`ID_Proveedor`) REFERENCES `Proveedor` (`ID_Proveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Usuario`
+--
+
+DROP TABLE IF EXISTS `Usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Usuario` (
+  `ID_Usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `Password` varchar(255) NOT NULL,
+  `Estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID_Usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Venta`
+--
+
+DROP TABLE IF EXISTS `Venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Venta` (
+  `ID_Venta` int(11) NOT NULL AUTO_INCREMENT,
+  `Fecha` date NOT NULL,
+  `Hora` time NOT NULL,
+  `Monto` float NOT NULL,
+  `Estado` varchar(30) DEFAULT NULL,
+  `Forma_Pago` varchar(20) DEFAULT NULL,
+  `ID_Caja` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Venta`),
+  KEY `ID_Caja` (`ID_Caja`),
+  CONSTRAINT `1` FOREIGN KEY (`ID_Caja`) REFERENCES `Caja` (`ID_Caja`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2026-04-09 22:48:25
