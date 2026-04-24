ff46a7de48e03e141369f4855ee87aee3e475ec3
Vitascom250306 <alex.tarin.gtz@gmail.com>
2026-04-24 15:02:03 -0600

    Feat: Fix navbar to the top

--- src/renderer/components/TopBar.jsx
+++ src/renderer/components/TopBar.jsx
@@ -12,15 +12,18 @@ function TopBar() {
   }, []);
 
   return (
-    <header className="w-full max-h-12 min-h-12 bg-gray-800 text-white px-5 flex items-center justify-between">
+    <>
+      <header className="fixed top-0 left-0 right-0 z-50 h-12 bg-gray-800 text-white px-5 flex items-center justify-between">
         <div className="flex-1 text-left">
-            <span className="font-bold">Estado de caja: TEST</span>
+          <span className="font-bold">Estado de caja: TEST</span>
         </div>
         <div className="flex-1 text-center">
-            <span className="font-extrabold text-3xl">Abarrotes "La bamba"</span>
+          <span className="font-extrabold text-3xl">Abarrotes "La bamba"</span>
         </div>
         <div className="flex-1 text-right font-bold">{dateTime}</div>
-    </header>
+      </header>
+      <div className="h-12 shrink-0" />
+    </>
   );
 }
 
