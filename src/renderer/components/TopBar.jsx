5d63604c045e0396fd8082dd6a5474ba6085c8f2
Vitascom250306 <alex.tarin.gtz@gmail.com>
2026-04-27 12:40:50 -0600

    Fix: Modify header layout

--- src/renderer/components/TopBar.jsx
+++ src/renderer/components/TopBar.jsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 //import logo from "../assets/logo.png"; // para el logo de la tienda, en su caso
 
 export default function TopBar() {
-  const [dateTime, setDateTime] = useState(new Date().toLocaleString());
+  const [dateTime, setDateTime] = useState(new Date().toLocaleString())
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setDateTime(new Date().toLocaleString());
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
+      setDateTime(new Date().toLocaleString())
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 h-12 bg-gray-800 text-white px-5 flex items-center justify-between">
-        <div className="flex-1 text-left">
-          <span className="font-bold">Estado de caja: TEST</span>
+      <header className="fixed top-0 left-0 right-0 z-50 h-22 md:h-12 bg-gray-800 text-white px-5 flex items-center justify-between">
+        <div className="text-left w-fit">
+          <span className="font-bold w-full">Estado de caja: TEST</span>
         </div>
-        <div className="flex-1 text-center">
+        <div className="text-center w-auto">
           <span className="font-extrabold text-3xl">Abarrotes "La bamba"</span>
         </div>
-        <div className="flex-1 text-right font-bold">{dateTime}</div>
+        <div className="text-right font-bold w-fit">{dateTime}</div>
       </header>
-      <div className="h-12 shrink-0" />
+      <div className="h-22 lg:h-12 shrink-0" />
     </>
-  );
+  )
 }
+
